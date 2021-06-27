411b4aa3d5bd8154e23e1bfafb8bc0e3f3595aa3
Josh <github@fayer.me>
2021-06-26 23:43:32 -0400

    Added support for social meta tags

---
+++ server/chess-cli.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+
+
+
+app.listen(3069, () => {
+    console.log('Listening on port 3069');
+})
